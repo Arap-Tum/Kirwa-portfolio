a696fd3587db882a9053e4a41590d29b8a6ddb4a
Arap-Tum <tumkibet32@gmail.com>
2025-04-01 07:43:46 +0300

    initial commet

---
+++ scripts/sidebar.js
@@ -0,0 +1,13 @@
+
+function displaSidebar() {
+    const menu = document.querySelector('.small-screen-menu');
+    const  sideBar =  document.querySelector('.side-bar');
+
+console.log('hello')
+    menu.addEventListener
+}
+
+menu.addEventListener('click', () => {
+ sideBar.style.display ='block';
+ console.log('dispayd sidebar')
+})
