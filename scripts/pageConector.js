365fcd827a6888e9c249b7af5e1c47a527cda2c2
Arap-Tum <tumkibet32@gmail.com>
2025-04-04 09:54:02 +0300

    NECESSARRY CHANGES

--- scripts/pageConector.js
+++ scripts/pageConector.js
@@ -1,27 +1,13 @@
-fetch('top-bar.html')
+    fetch('top-bar.html')
     .then(res => res.text())
     .then(data => {
         document.querySelector(".top-bar").innerHTML = data;
     })
 
 
-fetch ("footer.html") 
+   fetch ("footer.html") 
     .then(res => res.text())
     .then(data => {
         document.querySelector(".footer").innerHTML = data;
     });
-
-// side-bar display 
-
-document.addEventListener("DOMContentLoaded", function () {
-    function displaySidebar() {
-        const toggle = document.querySelector('.toggle');
-        const sidebar = document.querySelector('.side-bar');
-
-        menubar.addEventListener('click', () => {
-            sidebar.classList.toggle('active');
-        });
-
-        console.log("SIDEBAR function initialized")
-    }
-})
+ 
